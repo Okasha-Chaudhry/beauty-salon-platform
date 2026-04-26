53a228bb1daf075c8e4464812bb9cc963009de14
Okasha-Chaudhry <okashachaudhry01@gmail.com>
2026-04-27 03:30:36 +0500

    Try port 465 for Brevo

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 // Create transporter
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
+  port: 465,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
